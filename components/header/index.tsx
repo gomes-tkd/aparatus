26b59957940cd85187d848983557d962e85a8e61
Jose Gomes <jgomestkd@gmail.com>
2025-11-13 12:29:43 -0300

    feat: refactoring header component

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,29 +1,43 @@
 "use client";
-
-import { useState } from "react";
-import { SidebarMenu } from "@/components/sidebar-menu";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import SidebarMenu from "@/components/sidebar-menu";
 import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
+import { MenuIcon, MessageCircleIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import LogoSVG from "@/public/logo.svg";
 
-export function Header() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
+export default function Header() {
   return (
-    <>
-      <header>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => setIsSidebarOpen(true)}
-        >
-          <Menu className="h-5 w-5" />
+    <header className={"flex items-center justify-between bg-white px-5 py-6"}>
+      <Image src={LogoSVG} alt={"Aparatus Logo"} width={100} height={26.09} />
+      <div className={"flex items-center gap-2"}>
+        <Button variant={"outline"} size={"icon"} asChild>
+          <Link href={"/chat"}>
+            <MessageCircleIcon />
+          </Link>
         </Button>
-      </header>
-
-      <SidebarMenu 
-        isOpen={isSidebarOpen} 
-        onOpenChange={setIsSidebarOpen} 
-      />
-    </>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant={"outline"} size={"icon"}>
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className={"w-[370px] p-0"}>
+            <SheetHeader className={"border-b px-5 py-6 text-left"}>
+              <SheetTitle className={"text-lg font-bold"}>Menu</SheetTitle>
+            </SheetHeader>
+            <SidebarMenu />
+          </SheetContent>
+        </Sheet>
+      </div>
+    </header>
   );
 }
