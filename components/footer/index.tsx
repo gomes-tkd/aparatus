d9cb7fe5df02c6a0ca2c5a930088cef4ec878214
Jose Gomes <jgomestkd@gmail.com>
2025-11-13 12:36:26 -0300

    feat: add bookings page

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="bg-muted p-7 text-center">
-      <p className="text-foreground text-xs font-semibold">
+    <footer className={"bg-muted p-7 text-center"}>
+      <p className={"text-foreground text-xs font-semibold"}>
         &copy; 2025 Aparatus
       </p>
-      <p className="text-muted-foreground text-xs">
+      <p className={"text-muted-foreground text-xs"}>
         All rights reserved.
       </p>
     </footer>
