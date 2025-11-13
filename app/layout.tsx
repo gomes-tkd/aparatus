7565e003aefce3108e55279a5b3d754264be6209
Jose Gomes <jgomestkd@gmail.com>
2025-11-12 23:22:44 -0300

    feat: add footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { PageContainer } from "@/components/page-container";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <PageContainer>
           {children}
         </PageContainer>
+        <Footer />
       </body>
     </html>
   );
---
+++ components/footer/index.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-muted p-7 text-center">
+      <p className="text-foreground text-xs font-semibold">
+        &copy; 2025 Aparatus
+      </p>
+      <p className="text-muted-foreground text-xs">
+        All rights reserved.
+      </p>
+    </footer>
+  );
+}
